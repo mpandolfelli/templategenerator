281f0d6306b94d076418f4146d05068504d171f2
martinpandolfelli@gmail.com <martinpandolfelli@gmail.com>
2015-11-19 15:07:42 -0300

    se le puede setear animacion

--- template.js
+++ template.js
@@ -1,11 +1,13 @@
 var template = (function() {
   var pathLoad;
   var newHtml;
+  var animation = false;
+  var animation_type;
+  var speed = 1000;
 
   return {
     setPath: function(path){
       pathLoad = path;
-      console.log('Path to load: '+path);
     },
     load: function(template, target, data, callback){
     	var dataToReplace = data.data;
@@ -62,19 +64,35 @@ var template = (function() {
 
 					var tag = '<%'+index+'%>';
 					var regex = new RegExp(tag, "g");
-					console.log('index '+regex+' - ');
 					newHtml = newHtml.replace(regex, dataToReplace[index]);
 									  
 				} 
 			}
-			//muestro el html en donde corresponda
-			$(target).append(newHtml);
+			
+			if(animation){
+				if(animation_type == 'fade'){
+					$(target).append(newHtml).hide().fadeIn(speed);
+				}else if(animation_type == 'slide'){
+					$(target).append(newHtml).hide().SlideDown(speed);
+				}else{
+					console.log('El tipo de animacion es incorrecto, por favor use fade o slide');
+				}
+			}else{
+				$(target).append(newHtml);
+			}			
 
 			//eecuto el callback
 			callback;
 		}).fail(function() {
 			console.log("Hubo un error al cargar el template: " + template );
 		});
+    },
+    setAnimation: function(type, speed){
+    	//type: fade, slide
+    	//speed: number (default 1000)
+    	animation = true;
+    	animation_type = type;
+    	speed = speed;
     }
   };
 })();
